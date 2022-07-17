80b1477e6df7ade1bcc19ac0b35c98a5a9d75a6b
AztekTR <rubickaghanims@gmail.com>
2022-07-17 15:40:30 +0300

    fix: style on upload image

--- src/pages/RegisterPage/RegisterPage.styled.ts
+++ src/pages/RegisterPage/RegisterPage.styled.ts
@@ -1,38 +1,46 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
-import { HiPhotograph } from 'react-icons/hi'
+import { IoMdCloudUpload } from 'react-icons/io'
 
 export const Wrapper = styled.div(
   css({
     display: 'flex',
     alignItems: 'center',
+
     '.half': {
       flex: '1 0 50%',
     },
+
     '.form': {
       padding: '50px',
       maxWidth: '400px',
       margin: '0 auto',
     },
+
     '.view': {
       height: 'calc(100vh - 56px)',
       backgroundImage: `url(img/tree.jpg)`,
       backgroundPosition: 'center center',
     },
+
     '.title, p': {
       textAlign: 'center',
     },
+
     '.form-item': {
       display: 'block',
       marginBottom: '10px',
     },
+
     '.upload-file-wrapper': {
       display: 'flex',
       justifyContent: 'center',
       width: '100%',
-      border: '1px dashed #e4e4e4',
+      marginTop: '15px',
+      marginBottom: '-12px',
+      border: '2px dashed #ccebff',
       borderRadius: '6px',
-      padding: '2rem',
+      padding: '2.5rem 2rem',
       boxSizing: 'border-box',
       cursor: 'pointer',
     },
@@ -43,6 +51,7 @@ export const Wrapper = styled.div(
 
     '.file-label': {
       marginBottom: '.5rem',
+      color: '#666666',
     },
 
     '@media screen and (max-width: 900px)': {
@@ -53,10 +62,10 @@ export const Wrapper = styled.div(
   })
 )
 
-export const PictureIcon = styled(HiPhotograph)(
+export const PictureIcon = styled(IoMdCloudUpload)(
   css({
     width: '30px',
     height: '30px',
-    color: '#b3e0ff',
+    color: '#80ccff',
   })
 )
