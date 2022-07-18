b64b2e7b3a1d7143acf97031e119926c3bae4449
MapIIeX <podhorhyi_2002@ukr.net>
2022-07-18 19:26:45 +0300

    feat(users-page): test

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { User, users } from '../../consts/users'
+import { User, users } from '../../constants/users'
 import { ReturnButton, Wrapper } from './UserPage.styled'
 
 function UserPage() {
--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { users } from '../../consts/users'
+import { users } from '../../constants/users'
 import { DeleteButton, Wrapper } from './UsersPage.styled'
+import API from '../../shared/services/api'
 
 function UsersPage() {
+  useEffect(() => {
+      const formData = new FormData()
+
+      formData.append('username', 'zalupa529@gmail.com')
+      formData.append('password', 'wasd12345')
+
+      API.post('login', formData)
+  }, [])
+
   return (
     <Wrapper>
       <div className="wrapper">
         {users.map((el) => (
-          <Link to={`/users/${el.id}`} className="custom-card">
+          <Link key={el.id} to={`/users/${el.id}`} className="custom-card">
             <DeleteButton />
             <div className="image-wrapper">
               <img
